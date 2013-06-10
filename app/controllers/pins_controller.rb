6b46377012ae88c6f88b80c8138997701d3c6e43
Joshua Zuniga <joshazuniga@gmail.com>
2013-06-09 23:54:44 -0400

    trying to fix heroku

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -4,7 +4,7 @@ class PinsController < ApplicationController
   # GET /pins
   # GET /pins.json
 def index
-    @pins = Pin.order("created_at desc").page(params[:page]).per_page(155)
+    @pins = Pin.order("created_at desc").page(params[:page]).per_page(50)
 
     respond_to do |format|
       format.html # index.html.erb
