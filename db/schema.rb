1beb2a0b2bbc5c4b8940a0d694c004b99c8c2c5b
Joshua Zuniga <joshazuniga@gmail.com>
2013-06-12 21:51:55 -0400

    started adding admin controls

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130428203003) do
+ActiveRecord::Schema.define(:version => 20130612011429) do
 
   create_table "pins", :force => true do |t|
     t.string   "description"
@@ -29,8 +29,8 @@ ActiveRecord::Schema.define(:version => 20130428203003) do
   add_index "pins", ["user_id"], :name => "index_pins_on_user_id"
 
   create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "email",                  :default => "",    :null => false
+    t.string   "encrypted_password",     :default => "",    :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -39,9 +39,10 @@ ActiveRecord::Schema.define(:version => 20130428203003) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.datetime "created_at",                                :null => false
+    t.datetime "updated_at",                                :null => false
     t.string   "name"
+    t.boolean  "admin",                  :default => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
